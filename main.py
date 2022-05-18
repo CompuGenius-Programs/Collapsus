d64105dd455985904883b295b0c7047168d21cf0
CompuGenius Programs <contact@cgprograms.com>
2022-05-18 16:31:34 -0400

    Added /grotto_info

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import json
 import os
 
+import aiohttp
 import discord
 import dotenv
 from discord import Option
+from discord.ext.pages import Paginator, Page
+from parsel import Selector
 
 import parsers
 
@@ -17,6 +20,8 @@ quests_channel = 766039065849495574
 
 logo_url = "https://cdn.discordapp.com/emojis/856330729528361000.png"
 website_url = "https://dq9.carrd.co"
+
+grotto_search_url = "https://www.yabd.org/apps/dq9/grottosearch.php"
 
 
 @bot.event
@@ -35,7 +40,8 @@ async def help(ctx):
     description = '''
     A bot created by <@496392770374860811> for The Quester's Rest.
 
-    /quest_info [1-184] | Displays all info for a specific quest
+    /quest_info [Quest Number (1-184)] | Displays all info for a specific quest
+    /grotto_info [Material (Ex. Granite)] [Environment (Ex. Tunnel)] [Suffix (Ex. Woe)] [Level (Ex. 1)] <Location (Ex. 05)>
     /help | Displays this message
     '''
 
@@ -93,6 +99,72 @@ async def quest_info(ctx, quest_number: Option(int, "Quest Number (1-184)", requ
     embed.add_field(name="Pre-reqs", value=quest.prerequisite, inline=False)
 
     await ctx.respond(embed=embed)
+
+
+@bot.slash_command(name="grotto_info", description="Sends info about a grotto.", guild_ids=[guild_id])
+async def quest_info(ctx,
+                     material: Option(str, "Material (Ex. Granite)", required=True),
+                     environment: Option(str, "Environment (Ex. Tunnel)", required=True),
+                     suffix: Option(str, "Suffix (Ex. Woe)", required=True),
+                     level: Option(int, "Level (Ex. 1)", required=True),
+                     location: Option(str, "Location (Ex. 05)", required=False)):
+    async with aiohttp.ClientSession() as session:
+        params = {
+            "search": "Search",
+            "prefix": str(parsers.grotto_prefixes.index(material.title()) + 1),
+            "envname": str(parsers.grotto_environments.index(environment.title()) + 1),
+            "suffix": str(parsers.grotto_suffixes.index(suffix.title()) + 1),
+            "level": str(level),
+            "loc": str(int(location, base=16))
+        }
+
+        async with session.get(grotto_search_url, params=params) as response:
+            text = await response.text()
+            selector = Selector(text=text)
+            selector.xpath('//div[@class="minimap"]').remove()
+            divs = selector.xpath('//div[@class="inner"]//text()')
+            grottos = divs.getall()
+
+            entries = []
+
+            for parsed in parsers.create_grotto(grottos):
+                special = parsers.is_special(parsed)
+                color = discord.Color.gold() if special else discord.Color.green()
+                embed = create_embed(None, None, color)
+
+                if special:
+                    parsed = parsed[1:]
+
+                zipped = zip(range(len(parsed)), parsers.grotto_keys, parsed)
+
+                for i, key, value in zipped:
+                    if key == "Name":
+                        if special:
+                            value = ":star: %s :star:" % value
+                        embed.title = value
+                    else:
+                        if key == "Chests (S - I)":
+                            ranks = ["**S**", "**A**", "**B**", "**C**", "**D**", "**E**", "**F**", "**G**", "**H**", "**I**"]
+                            values = [str(x) for x in parsed[i:i + 10]]
+                            chests = list(zip(ranks, values))
+                            value = ", ".join([': '.join(x) for x in chests])
+                        embed.add_field(name=key, value=value, inline=False)
+                embed.url = str(response.url)
+                entries.append(embed)
+
+            if len(entries) == 1:
+                embed = entries[0]
+            elif len(entries) == 0:
+                embed = create_embed("Invalid Grotto", None, discord.Color.green())
+
+        if len(entries) > 1:
+            pages = []
+            for entry in entries:
+                pages.append(Page(embeds=[entry]))
+            paginator = Paginator(pages=pages)
+            await paginator.respond(ctx.interaction)
+        else:
+            await ctx.respond(embed=embed)
 
 
 def create_embed(title, description, color, footer="© CompuGenius Programs. All rights reserved.", image="", *, url="",
--- parsers.py
+++ parsers.py
@@ -1,5 +1,6 @@
 import re
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Dict
 
 from dataclasses_json import dataclass_json
 
@@ -18,22 +19,176 @@ class Quest:
     prerequisite: str = ""
     repeat: bool = False
 
-    def print_data(self):
-        print("""\
-Number: %i
-Name: %s
-Story: %s
-Location: %s
-Request: %s
-Solution: %s
-Reward: %s
-Prerequisite: %s
-Repeat: %s
-""" % (self.number, self.name, self.story, self.location, self.request, self.solution, self.reward, self.prerequisite,
-       self.repeat))
 
+@dataclass_json
+@dataclass
+class Grotto:
+    """Class for a grotto."""
+    seed: str = ""
+    rank: str = ""
+    name: str = ""
+    boss: str = ""
+    type: str = ""
+    floors: int = 0
+    monster_rank: str = ""
+    chests: Dict[str, int] = field(default_factory=dict)
+    locations: int = 0
+    grotto_special: bool = False
+
+
+grotto_bosses = [
+    "Equinox",
+    "Nemean",
+    "Shogum",
+    "Trauminator",
+    "Elusid",
+    "Sir Sanguinus",
+    "Atlas",
+    "Hammibal",
+    "Fowleye",
+    "Excalipurr",
+    "Tyrannosaurus Wrecks",
+    "Greygnarl"
+]
+
+grotto_prefixes = [
+    "Clay",
+    "Rock",
+    "Granite",
+    "Basalt",
+    "Graphite",
+    "Iron",
+    "Copper",
+    "Bronze",
+    "Steel",
+    "Silver",
+    "Gold",
+    "Platinum",
+    "Ruby",
+    "Emerald",
+    "Sapphire",
+    "Diamond"
+]
+
+grotto_environments = [
+    "Cave",
+    "Tunnel",
+    "Mine",
+    "Crevasse",
+    "Marsh",
+    "Lair",
+    "Icepit",
+    "Lake",
+    "Crater",
+    "Path",
+    "Snowhall",
+    "Moor",
+    "Dungeon",
+    "Crypt",
+    "Nest",
+    "Ruins",
+    "Tundra",
+    "Waterway",
+    "World",
+    "Abyss",
+    "Maze",
+    "Glacier",
+    "Chasm",
+    "Void"
+]
+
+grotto_suffixes = [
+    "Joy",
+    "Bliss",
+    "Glee",
+    "Doubt",
+    "Woe",
+    "Dolour",
+    "Regret",
+    "Bane",
+    "Fear",
+    "Dread",
+    "Hurt",
+    "Gloom",
+    "Doom",
+    "Evil",
+    "Ruin",
+    "Death"
+]
+
+grotto_types = [
+    "Caves",
+    "Ruins",
+    "Ice",
+    "Water",
+    "Fire"
+]
+
+grotto_special = "Has a special floor"
+grotto_keys = ("Seed", "Rank", "Name",
+               "Boss", "Type", "Floors",
+               "Monster Rank", "Chests (S - I)")
 
 quests_regex = r'^(?:(?:(⭐) )?\*\*Quest #(\d+) - (.+)\*\*(?: \1)?(?:```yml)?|(\w+): (.+?)(?:```)?)$'
+grotto_cleanup_regex = r'([\w\d\.\-:/() ]+)'
+
+
+def _hex(value):
+    n = int(value)
+    if n <= 16:
+        return n
+    n = int(value, base=16)
+
+    hex_str = str.upper(hex(n)[2:])
+    return hex_str.zfill(4)
+
+
+def is_special(data: tuple):
+    try:
+        return data[0] == grotto_special
+    except ValueError:
+        return False
+
+
+def create_grotto(datas):
+    converters = (_hex, int, str)
+
+    magic = 17
+    ye = []
+
+    for data in datas:
+        match_found = re.compile(grotto_cleanup_regex).match(data)
+
+        if match_found:
+            stripped = str.strip(match_found.group(), "' ")
+
+            if stripped != "":
+                for i, converter in enumerate(converters):
+                    try:
+                        converted = converter(stripped)
+                    except Exception:
+                        converted = None
+                        continue
+                    else:
+                        if isinstance(converted, str):
+                            if converted.find(":") > -1:
+                                continue
+                            elif converted == grotto_special:
+                                magic = 18
+                                ye.insert(0, converted)
+                            else:
+                                ye.append(converted)
+                        else:
+                            ye.append(converted)
+                        break
+                    finally:
+                        if len(ye) == magic:
+                            yield tuple(ye)
+                            magic = 17
+                            ye = []
+
+    if len(ye) > 0:
+        yield tuple(ye)
 
 
 def parse_regex(type, string):
@@ -46,32 +201,33 @@ def parse_regex(type, string):
     # for match in matches:
     #     print(match)
 
-    number = int(matches[0][1])
-    name = matches[0][2]
-    story = matches[0][0] == "⭐"
+    if type == Quest:
+        number = int(matches[0][1])
+        name = matches[0][2]
+        story = matches[0][0] == "⭐"
 
-    location = ""
-    request = ""
-    solution = ""
-    reward = ""
-    prerequisite = ""
-    repeat = False
+        location = ""
+        request = ""
+        solution = ""
+        reward = ""
+        prerequisite = ""
+        repeat = False
 
-    for info in matches[1:]:
-        title = info[3].lower()
-        data = info[4].strip()
-        if title == "location":
-            location = data
-        if title == "request":
-            request = data
-        if title == "solution":
-            solution = data
-        if title == "reward":
-            reward = data
-        if title == "prerequisite":
-            prerequisite = data
-        if title == "repeat":
-            repeat = data.lower() == "yes"
+        for info in matches[1:]:
+            title = info[3].lower()
+            data = info[4].strip()
+            if title == "location":
+                location = data
+            if title == "request":
+                request = data
+            if title == "solution":
+                solution = data
+            if title == "reward":
+                reward = data
+            if title == "prerequisite":
+                prerequisite = data
+            if title == "repeat":
+                repeat = data.lower() == "yes"
 
-    quest = Quest(number, name, story, location, request, solution, reward, prerequisite, repeat)
-    return quest.to_dict()
+        quest = Quest(number, name, story, location, request, solution, reward, prerequisite, repeat)
+        return quest.to_dict()

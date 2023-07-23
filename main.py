0afeb57b7871dd939378faae1c0584447c6fecca
CompuGenius Programs <contact@cgprograms.com>
2023-07-22 22:32:38 -0400

    Added stopping the bot, playing all songs and proper handling of the bot already playing a song.

--- main.py
+++ main.py
@@ -120,37 +120,90 @@ async def get_songs(ctx: discord.AutocompleteContext):
 
 @bot.slash_command(name="song", description="Plays a song.")
 async def _song(ctx, song_name: Option(str, "Song Name", autocomplete=get_songs, required=True)):
-    voice_state = ctx.author.voice
-    if voice_state is None:
-        embed = create_embed("You need to be in a voice channel to use this command.")
+    voice_client = discord.utils.get(bot.voice_clients, guild=bot.get_guild(guild_id))
+    if voice_client is None or not voice_client.is_playing():
+        voice_state = ctx.author.voice
+        if voice_state is None:
+            embed = create_embed("You need to be in a voice channel to use this command.")
+            return await ctx.respond(embed=embed, ephemeral=True)
+
+        channel = voice_state.channel.id
+        if channel == stream_channel:
+            embed = create_embed("You can't use this command in the stream channel.")
+            return await ctx.respond(embed=embed, ephemeral=True)
+
+        await ctx.defer()
+
+        with open("songs.json", "r", encoding="utf-8") as fp:
+            data = json.load(fp)
+        songs = data["songs"]
+
+        index = next(filter(lambda s: s["title"].lower() == song_name.lower(), songs), None)
+        song = parsers.Song.from_dict(index)
+
+        if voice_client is None:
+            voice_client = await bot.get_channel(channel).connect()
+        await play(ctx, voice_client, song, channel)
+
+        while voice_client.is_playing():
+            await sleep(1)
+        await voice_client.disconnect()
+    else:
+        embed = create_embed("I'm already playing a song. Please wait until it's finished.")
         return await ctx.respond(embed=embed, ephemeral=True)
 
-    channel = voice_state.channel.id
-    if channel == stream_channel:
-        embed = create_embed("You can't use this command in the stream channel.")
+
+@bot.slash_command(name="all_songs", description="Plays all songs.")
+async def _all_songs(ctx):
+    voice_client = discord.utils.get(bot.voice_clients, guild=bot.get_guild(guild_id))
+    if voice_client is None or not voice_client.is_playing():
+        voice_state = ctx.author.voice
+        if voice_state is None:
+            embed = create_embed("You need to be in a voice channel to use this command.")
+            return await ctx.respond(embed=embed, ephemeral=True)
+
+        channel = voice_state.channel.id
+        if channel == stream_channel:
+            embed = create_embed("You can't use this command in the stream channel.")
+            return await ctx.respond(embed=embed, ephemeral=True)
+
+        await ctx.defer()
+
+        with open("songs.json", "r", encoding="utf-8") as fp:
+            data = json.load(fp)
+        songs = data["songs"]
+
+        for s in songs:
+            song = parsers.Song.from_dict(s)
+            if voice_client is None:
+                voice_client = await bot.get_channel(channel).connect()
+            await play(ctx, voice_client, song, channel)
+            while voice_client.is_playing():
+                await sleep(1)
+        await voice_client.disconnect()
+    else:
+        embed = create_embed("I'm already playing a song. Please wait until it's finished.")
         return await ctx.respond(embed=embed, ephemeral=True)
 
-    await ctx.defer()
 
-    with open("songs.json", "r", encoding="utf-8") as fp:
-        data = json.load(fp)
-    songs = data["songs"]
+@bot.slash_command(name="stop", description="Stops playing a song.")
+async def _stop_song(ctx):
+    voice_client = discord.utils.get(bot.voice_clients, guild=bot.get_guild(guild_id))
+    if voice_client is None or not voice_client.is_playing():
+        embed = create_embed("I'm not playing a song.")
+        return await ctx.respond(embed=embed, ephemeral=True)
 
-    index = next(filter(lambda s: s["title"].lower() == song_name.lower(), songs), None)
-    song = parsers.Song.from_dict(index)
-    await play(ctx, song, channel)
+    await voice_client.disconnect()
+
+    embed = create_embed("Stopped playing song.")
+    await ctx.respond(embed=embed)
 
 
-async def play(ctx, song: parsers.Song, channel):
+async def play(ctx, voice_client, song: parsers.Song, channel):
     source = discord.FFmpegPCMAudio(song.url, executable="ffmpeg")
-    voice_client = await bot.get_channel(channel).connect()
     voice_client.play(source)
     embed = create_embed("Playing `%s`" % song.title)
     await ctx.followup.send(embed=embed)
-
-    while voice_client.is_playing():
-        await sleep(1)
-    await voice_client.disconnect()
 
 
 @bot.slash_command(name="parse_quests", description="Parses the quests.")

6c11e3e81acedb8160c9aa4416f2cfaac730d5d4
CompuGenius Programs <contact@cgprograms.com>
2023-07-28 14:49:00 -0400

    Fixed monster searching with autocorrect, made monster paginator select correct page if specified and fixed bug where monsters and recipes with - in their name wouldn't show the image.

--- main.py
+++ main.py
@@ -148,7 +148,6 @@ async def _song(ctx, song_name: Option(str, "Song Name", autocomplete=get_songs,
             voice_client = await bot.get_channel(channel).connect()
         await play(ctx, voice_client, song, channel)
 
-        channel = voice_state.channel.id
         embed = create_embed("Playing `%s` in <#%s>" % (song.title, channel))
         await ctx.followup.send(embed=embed)
 
@@ -187,7 +186,6 @@ async def _all_songs(ctx):
                 voice_client = await bot.get_channel(channel).connect()
             await play(ctx, voice_client, song, channel)
 
-            channel = voice_state.channel.id
             embed = create_embed("Playing all songs. Currently playing `%s` in <#%s>" % (song.title, channel))
             try:
                 await message.edit(embed=embed)
@@ -478,7 +476,7 @@ async def _recipe(ctx, creation_name: Option(str, "Creation (Ex. Special Medicin
             recipe_images_url = accessory_images_url
 
         if recipe_images_url != "":
-            recipe.image = recipe_images_url % clean_text(recipe.result, False)
+            recipe.image = recipe_images_url % clean_text(recipe.result, False, True)
     embed = create_embed(title, color=color, image=recipe.image)
 
     embed.add_field(name="Type", value=recipe.type, inline=False)
@@ -514,19 +512,31 @@ async def _monster(ctx,
         data = json.load(fp)
 
     monsters = data["monsters"]
-    if int_from_string(monster_identifier) == "":
+
+    monster_number = 0
+
+    if " - " in monster_identifier:
+        monster_number = monster_identifier.split(" - ")[0]
+        indexes = list(filter(lambda r: clean_text(r["name"].lower()) == clean_text(
+            monster_identifier.split(" - ")[1].lower()) or clean_text(
+            r.get("altname", "").lower()) == clean_text(monster_identifier.split(" - ")[1].lower()), monsters))
+    else:
         indexes = list(filter(
             lambda r: clean_text(r["name"].lower()) == clean_text(monster_identifier.lower()) or clean_text(
                 r.get("altname", "").lower()) == clean_text(
                 monster_identifier.lower()), monsters))
-    else:
-        indexes = list(filter(lambda r: int_from_string(r["number"]) == int_from_string(monster_identifier), monsters))
+        if not indexes:
+            monster_number = "#" + "%03d" % int_from_string(monster_identifier) + monster_identifier[-1] if \
+                monster_identifier[-1].isalpha() else ""
+            indexes = list(
+                filter(lambda r: int_from_string(r["number"]) == int_from_string(monster_identifier), monsters))
 
     if len(indexes) == 0:
         embed = create_embed("No monster found with the identifier `%s`. Please check spelling and try again."
                              % monster_identifier)
         return await ctx.respond(embed=embed)
 
+    proper_page = 0
     embeds = []
     for index in indexes:
         monster = parsers.Monster.from_dict(index)
@@ -554,14 +564,18 @@ async def _monster(ctx,
             description += "\n**__Drop 3__**\n%s\n" % titlecase(monster.drop3)
 
         if monster.image == "":
-            monster.image = monster_images_url % clean_text(monster.name, False)
+            monster.image = monster_images_url % clean_text(monster.name, False, True)
 
         embed = create_embed(title, description, image=monster.image)
         embeds.append(embed)
 
+        if monster_number != 0 and monster_number == monster.number:
+            proper_page = len(embeds) - 1
+
     if len(embeds) > 1:
         paginator = create_paginator(embeds, None)
         await paginator.respond(ctx.interaction)
+        await paginator.goto_page(proper_page)
     else:
         await ctx.respond(embed=embeds[0])
 
@@ -841,9 +855,10 @@ def int_from_string(string):
         return ""
 
 
-def clean_text(text, remove_spaces=True):
-    text = text.lower().replace("'", "").replace("’", "").replace("-", "").replace("ñ", "n").replace("ó", "o").replace(
+def clean_text(text, remove_spaces=True, url=False):
+    text = text.lower().replace("'", "").replace("’", "").replace("ñ", "n").replace("ó", "o").replace(
         ".", "")
+    text = text.replace("-", "_") if url else text.replace("-", "")
     if remove_spaces:
         text = text.replace(" ", "")
     else:

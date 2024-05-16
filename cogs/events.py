8ade1479750142f651c4f5dd53572be889cd42ac
CompuGenius <contact@cgprograms.com>
2024-05-15 20:42:17 -0400

    Created birthday command 🎉

---
+++ cogs/events.py
@@ -0,0 +1,46 @@
+import random
+from datetime import datetime, timedelta
+
+import discord
+from discord.ext import commands
+
+
+def setup(bot):
+    bot.add_cog(Events(bot))
+
+
+def is_birthday_week():
+    today = datetime.now()
+    birthday = datetime(today.year, 5, 13)
+    start = birthday - timedelta(days=birthday.weekday())
+    end = start + timedelta(days=6)
+    return start <= today <= end
+
+
+class Events(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.party_role = 1240460078528725014
+        self.party_popper = "https://www.woodus.com/den/gallery/graphics/dq9ds/item/party_popper.png"
+
+    @commands.Cog.listener()
+    async def on_application_command(self, command):
+        role = discord.utils.get(command.guild.roles, id=self.party_role)
+        if role is not None:
+            if is_birthday_week():
+                await command.author.add_roles(role)
+            else:
+                await command.author.remove_roles(role)
+
+    @discord.slash_command()
+    async def happy_birthday(self, ctx):
+        age = datetime.now().year - self.bot.user.created_at.year
+        message = random.choice(["Thank you so much! I can't believe I've been helping people for {age} years already!",
+                                 "It's only been {age} years? It feels like you guys have been bothering me forever!",
+                                 "Wow, {age} years already? Time flies when you're having fun!",
+                                 "I can't believe I'm {age} years old already! I'm getting so old!",
+                                 "I'm {age} years old now? I hope bots age like fine wine!",
+                                 "{age} years. Wow. I hope Discord bots don't get rusty like other bots!"])
+        embed = discord.Embed(title="Happy Birthday!", description=message.format(age=age), color=discord.Color.gold(),
+                              image=self.party_popper)
+        await ctx.respond(embed=embed)
